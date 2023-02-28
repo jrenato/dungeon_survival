35c78a8c66514fe7f8ac6dbf598ad2126442eae5
karvalho <renato@karvalho.com.br>
2023-02-28 17:11:23 -0300

    Added game camera

---
+++ src/camera/game_camera.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+var target_position : Vector2 = Vector2.ZERO
+
+
+func _ready() -> void:
+	make_current()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	acquire_target()
+	if target_position:
+		global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 10))
+
+
+func acquire_target() -> void:
+	var player_nodes = get_tree().get_nodes_in_group("player")
+	if player_nodes.size() > 0:
+		var player = player_nodes[0] as Player
+		target_position = player.global_position
--- src/player/player.gd
+++ src/player/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 const MAX_SPEED : float = 200.0
 
