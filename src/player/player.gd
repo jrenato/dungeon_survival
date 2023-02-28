220e1ea10a0706b2fe906de00ed233ce0b2064e9
karvalho <renato@karvalho.com.br>
2023-02-28 16:33:39 -0300

    Added simple player and basic level layout

---
+++ src/player/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const MAX_SPEED : float = 200.0
+
+
+func _process(delta: float) -> void:
+	var direction : Vector2 = get_movement_vector()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+
+func get_movement_vector() -> Vector2:
+	return Input.get_vector("move_left", 'move_right', "move_up", "move_down")
