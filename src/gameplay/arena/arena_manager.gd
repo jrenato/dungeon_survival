8efddbb0c149ddf8637f0e350fd740b68d4d2920
karvalho <renato@karvalho.com.br>
2023-03-04 15:52:24 -0300

    Increasing spawn time on dificulty change

--- src/gameplay/arena/arena_manager.gd
+++ src/gameplay/arena/arena_manager.gd
@@ -1,15 +1,26 @@
 extends Node
 class_name ArenaManager
 
+signal arena_difficulty_changed(arena_difficulty : int)
+
+const DIFFICULTY_INTERVAL : int = 5
 
 @export var end_screen_scene : PackedScene
 
-
 @onready var timer: Timer = %Timer
+
+var arena_difficulty : int = 1
 
 
 func _ready() -> void:
 	timer.timeout.connect(on_timer_timeout)
+
+
+func _process(delta: float) -> void:
+	var next_time_target = timer.wait_time - (arena_difficulty * DIFFICULTY_INTERVAL)
+	if timer.time_left <= next_time_target:
+		arena_difficulty += 1
+		arena_difficulty_changed.emit(arena_difficulty)
 
 
 func get_time_elapsed() -> float:
--- src/gameplay/managers/enemy_manager.gd
+++ src/gameplay/managers/enemy_manager.gd
@@ -3,13 +3,22 @@ extends Node
 const SPAWN_RADIUS : int = 400
 
 @export var enemy_scene : PackedScene
+@export var arena_manager : ArenaManager
+
+@onready var timer: Timer = %Timer
+
+var base_spawn_time = 0
 
 
 func _ready() -> void:
-	%Timer.timeout.connect(on_timer_timeout)
+	base_spawn_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_manager.arena_difficulty_changed.connect(_on_arena_difficulty_changed)
 
 
 func on_timer_timeout() -> void:
+	timer.start()
+
 	var player : Player = get_tree().get_first_node_in_group("player") as Player
 	if player == null:
 		return
@@ -22,3 +31,10 @@ func on_timer_timeout() -> void:
 	if entities_layer != null:
 		enemy.global_position = spawn_position
 		entities_layer.add_child(enemy)
+
+
+func _on_arena_difficulty_changed(arena_difficulty : int) -> void:
+	var time_off : float = (0.1 / 12) * arena_difficulty
+	time_off = min(time_off, 0.7)
+	print("time_off: ", time_off)
+	timer.wait_time = base_spawn_time - time_off
