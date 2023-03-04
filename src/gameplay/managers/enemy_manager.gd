b462d642962f0a0628b071d073b3dda3a9500cf8
karvalho <renato@karvalho.com.br>
2023-03-04 18:27:22 -0300

    Fixed enemy spawning in collision tiles

--- src/gameplay/managers/enemy_manager.gd
+++ src/gameplay/managers/enemy_manager.gd
@@ -12,24 +12,40 @@ var base_spawn_time = 0
 
 func _ready() -> void:
 	base_spawn_time = timer.wait_time
-	timer.timeout.connect(on_timer_timeout)
+	timer.timeout.connect(_on_timer_timeout)
 	arena_manager.arena_difficulty_changed.connect(_on_arena_difficulty_changed)
 
 
-func on_timer_timeout() -> void:
+func get_spawn_position(player : Player) -> Vector2:
+	var spawn_position = Vector2.ZERO
+	var random_direction : Vector2 = Vector2.RIGHT.rotated(randf_range(0, TAU))
+
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+
+		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+
+		if result.is_empty():
+			break
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+
+	return spawn_position
+
+
+func _on_timer_timeout() -> void:
 	timer.start()
 
 	var player : Player = get_tree().get_first_node_in_group("player") as Player
 	if player == null:
 		return
 
-	var random_direction : Vector2 = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position : Vector2 = player.global_position + (random_direction * SPAWN_RADIUS)
-
 	var enemy : Enemy = enemy_scene.instantiate() as Enemy
+
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	if entities_layer != null:
-		enemy.global_position = spawn_position
+		enemy.global_position = get_spawn_position(player)
 		entities_layer.add_child(enemy)
 
 
