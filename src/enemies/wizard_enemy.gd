f9fa0987c165f3cd9f21e62e557163b50ad4d775
karvalho <renato@karvalho.com.br>
2023-03-05 12:53:11 -0300

    Improved wizard animation and movement

--- src/enemies/wizard_enemy.gd
+++ src/enemies/wizard_enemy.gd
@@ -5,11 +5,21 @@ class_name WizardEnemy
 @onready var velocity_component: VelocityComponent = $VelocityComponent as VelocityComponent
 @onready var visuals: Node2D = $Visuals
 
+var is_moving : bool = false
+
 
 func _process(delta: float) -> void:
-	velocity_component.accelerate_to_player()
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else:
+		velocity_component.decelerate()
+
 	velocity_component.move(self)
 	
 	var move_sign : int = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(move_sign, 1)
+
+
+func set_is_moving(moving : bool):
+	is_moving = moving
--- src/gameplay/components/velocity_component.gd
+++ src/gameplay/components/velocity_component.gd
@@ -26,6 +26,10 @@ func accelerate_in_direction(direction : Vector2):
 	velocity = velocity.lerp(desired_velocity, weight)
 
 
+func decelerate() -> void:
+	accelerate_in_direction(Vector2.ZERO)
+
+
 func move(character_body : CharacterBody2D):
 	character_body.velocity = velocity
 	character_body.move_and_slide()
