b8a8021de44521bc8b4a67603edd0827f4f4d808
karvalho <renato@karvalho.com.br>
2023-03-05 11:16:38 -0300

    Added walk and death animation to enemy

--- src/enemies/enemy.gd
+++ src/enemies/enemy.gd
@@ -4,12 +4,17 @@ class_name Enemy
 
 const MAX_SPEED : int = 40
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var visuals: Node2D = $Visuals
 
 
 func _process(delta: float) -> void:
 	var direction : Vector2 = get_direction_to_player()
 	velocity = direction * MAX_SPEED
 	move_and_slide()
+	
+	var move_sign : int = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(-move_sign, 1)
 
 
 func get_direction_to_player() -> Vector2:
---
+++ src/gameplay/components/death_component.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+
+@export var health_component : HealthComponent
+@export var sprite : Sprite2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+
+
+func _ready() -> void:
+	set_particles_texture()
+	health_component.died.connect(_on_died)
+
+
+func set_particles_texture():
+	var atlas_texture : AtlasTexture = AtlasTexture.new()
+	atlas_texture.atlas = sprite.texture
+	if sprite.region_enabled:
+		atlas_texture.region = sprite.region_rect
+	gpu_particles_2d.texture = atlas_texture
+
+
+func _on_died() -> void:
+	if owner == null || not owner is Node2D:
+		return
+
+	var spawn_position : Vector2 = owner.global_position
+
+	var entities_layer : Node = get_tree().get_first_node_in_group("entities_layer")
+	get_parent().remove_child(self)
+	entities_layer.add_child(self)
+
+	global_position = spawn_position
+	animation_player.play("default")
