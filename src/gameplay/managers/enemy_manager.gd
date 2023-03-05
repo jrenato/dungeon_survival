1a5e367d818e1e57eb87dc1bd49004607fcffaeb
karvalho <renato@karvalho.com.br>
2023-03-05 12:21:08 -0300

    Added weighted table for enemy spawn

--- src/gameplay/managers/enemy_manager.gd
+++ src/gameplay/managers/enemy_manager.gd
@@ -2,15 +2,19 @@ extends Node
 
 const SPAWN_RADIUS : int = 400
 
-@export var enemy_scene : PackedScene
 @export var arena_manager : ArenaManager
+@export var brown_rat_enemy_scene : PackedScene
+@export var wizard_enemy_scene : PackedScene
 
 @onready var timer: Timer = %Timer
 
 var base_spawn_time = 0
+var enemy_table = WeightedTable.new()
 
 
 func _ready() -> void:
+	enemy_table.add_item(brown_rat_enemy_scene, 10)
+	
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(_on_timer_timeout)
 	arena_manager.arena_difficulty_changed.connect(_on_arena_difficulty_changed)
@@ -41,6 +45,7 @@ func _on_timer_timeout() -> void:
 	if player == null:
 		return
 
+	var enemy_scene : PackedScene = enemy_table.pick_item()
 	var enemy : Node2D = enemy_scene.instantiate() as Node2D
 
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
@@ -53,3 +58,6 @@ func _on_arena_difficulty_changed(arena_difficulty : int) -> void:
 	var time_off : float = (0.1 / 12) * arena_difficulty
 	time_off = min(time_off, 0.7)
 	timer.wait_time = base_spawn_time - time_off
+
+	if arena_difficulty == 6:
+		enemy_table.add_item(wizard_enemy_scene, 20)
