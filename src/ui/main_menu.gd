626a669956ad17f435f0906b6a00a0010cbc8efd
renato <renato@karvalho.com.br>
2023-03-07 17:38:22 -0300

    Added options menu

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -1,8 +1,11 @@
 extends CanvasLayer
+class_name MainMenu
 
 @onready var play_button: Button = %PlayButton
 @onready var options_button: Button = %OptionsButton
 @onready var quit_button: Button = %QuitButton
+
+var options_menu_scene : PackedScene = preload("res://src/ui/options_menu.tscn")
 
 
 func _ready() -> void:
@@ -16,8 +19,14 @@ func _on_play_button_pressed() -> void:
 
 
 func _on_options_button_pressed() -> void:
-	pass
+	var options_menu : OptionsMenu = options_menu_scene.instantiate() as OptionsMenu
+	add_child(options_menu)
+	options_menu.back_button.pressed.connect(_on_options_close.bind(options_menu))
 
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_close(options_menu : OptionsMenu) -> void:
+	options_menu.queue_free()
---
+++ src/ui/options_menu.gd
@@ -0,0 +1,54 @@
+extends CanvasLayer
+class_name OptionsMenu
+
+signal back_pressed
+
+@onready var window_button: Button = %WindowButton
+@onready var music_slider: HSlider = %MusicSlider
+@onready var sound_slider: HSlider = %SoundSlider
+@onready var back_button: Button = %BackButton
+
+
+func _ready() -> void:
+	window_button.pressed.connect(_on_window_button_pressed)
+	music_slider.value_changed.connect(_on_audio_slider_changed.bind("music"))
+	sound_slider.value_changed.connect(_on_audio_slider_changed.bind("sound"))
+	back_button.pressed.connect(_on_back_button_pressed)
+	update_display()
+
+
+func update_display() -> void:
+	window_button.text = "Windowed"
+	if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		window_button.text = "Fullscreen"
+	music_slider.value = get_bus_volume_percent("music")
+	sound_slider.value = get_bus_volume_percent("sound")
+
+
+func set_bus_volume_percent(bus_name : String, percent: float) -> void:
+	var bus_index : int = AudioServer.get_bus_index(bus_name)
+	var volume_db : float = linear_to_db(percent)
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
+
+func get_bus_volume_percent(bus_name : String) -> float:
+	var bus_index : int = AudioServer.get_bus_index(bus_name)
+	var volume_db : float = AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(volume_db)
+
+
+func _on_window_button_pressed() -> void:
+	var mode : int = DisplayServer.window_get_mode()
+	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	update_display()
+
+
+func _on_audio_slider_changed(value : float, bus_name : String) -> void:
+	set_bus_volume_percent(bus_name, value)
+
+
+func _on_back_button_pressed() -> void:
+	back_pressed.emit()
