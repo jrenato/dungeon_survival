98585aa447de62fed8840ca149b2cac2df2428d6
karvalho <renato@karvalho.com.br>
2023-03-07 21:29:20 -0300

    Implemented pause menu logic

--- src/ui/pause_menu.gd
+++ src/ui/pause_menu.gd
@@ -6,9 +6,18 @@ extends CanvasLayer
 @onready var options_button: Button = %OptionsButton
 @onready var quit_button: Button = %QuitButton
 
+var options_menu_scene : PackedScene = preload("res://src/ui/options_menu.tscn")
+
+var is_closing : bool = false
+
 
 func _ready() -> void:
 	resume_button.pressed.connect(_on_resume_button_pressed)
+	options_button.pressed.connect(_on_options_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+	get_tree().paused = true
+	panel_container.pivot_offset = panel_container.size / 2 
 	animation_player.play("default")
 
 	var tween = create_tween()
@@ -17,5 +26,44 @@ func _ready() -> void:
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("pause"):
+		close()
+		get_tree().root.set_input_as_handled
+
+
+func close() -> void:
+	if is_closing:
+		return
+
+	is_closing = true
+	animation_player.play_backwards("default")
+
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.0)
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0.3)\
+		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+
+	await tween.finished
+
+	get_tree().paused = false
+	queue_free()
+
+
 func _on_resume_button_pressed() -> void:
-	pass
+	close()
+
+
+func _on_options_button_pressed() -> void:
+	var options_menu : OptionsMenu = options_menu_scene.instantiate() as OptionsMenu
+	add_child(options_menu)
+	options_menu.back_button.pressed.connect(_on_options_close.bind(options_menu))
+
+
+func _on_options_close(options_menu : OptionsMenu) -> void:
+	options_menu.queue_free()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://src/ui/main_menu.tscn")
--- src/world/levels/level.gd
+++ src/world/levels/level.gd
@@ -1,15 +1,21 @@
 extends Node
 
-
 @export var end_screen_scene : PackedScene
 
-
 @onready var player: Player = %Player
+
+var pause_menu_scene : PackedScene = preload("res://src/ui/pause_menu.tscn")
 
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color("763b36"))
 	player.health_component.died.connect(_on_player_died)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
+		get_tree().root.set_input_as_handled()
 
 
 func _on_player_died() -> void:
