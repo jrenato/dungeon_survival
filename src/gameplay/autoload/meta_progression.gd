3949aee1503630c658ea4ae3e26bd62d0f09c9d1
karvalho <renato@karvalho.com.br>
2023-03-08 07:33:09 -0300

    Added saving and loading data

--- src/gameplay/autoload/meta_progression.gd
+++ src/gameplay/autoload/meta_progression.gd
@@ -1,6 +1,8 @@
 extends Node
 
-var save_data : Dictionary = {
+const SAVE_FILE_PATH = "user://game.save"
+
+var data : Dictionary = {
 	"coins": 0,
 	"meta_upgrades": {}
 }
@@ -8,15 +10,28 @@ var save_data : Dictionary = {
 
 func _ready() -> void:
 	GameEvents.experience_vial_collected.connect(_on_experience_collected)
+	load_data()
+
+
+func load_data():
+	if not FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+	var file : FileAccess = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	data = file.get_var()
+
+
+func save_data():
+	var file : FileAccess = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_var(data)
 
 
 func add_meta_upgrade(upgrade: MetaUpgrade) -> void:
-	if not save_data["meta_upgrades"].has(upgrade.id):
-		save_data["meta_upgrades"][upgrade.id] = {
+	if not data["meta_upgrades"].has(upgrade.id):
+		data["meta_upgrades"][upgrade.id] = {
 			"level": 0
 		}
-	save_data["meta_upgrades"][upgrade.id]["level"] += 1
+	data["meta_upgrades"][upgrade.id]["level"] += 1
 
 
 func _on_experience_collected(ammount: float) -> void:
-	save_data["coins"] += ammount
+	data["coins"] += ammount
